1b87b29d7a849122ed6cf4094f4d443a3c71853c
Nikita Chernyshov <xpom55@mail.ru>
2017-10-24 16:36:54 +0300

    146 l/ load data from firebase

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import { View, Text } from 'react-native';
+import { employeesFetch } from './../actions/EmployeeActions';
 
 class EmpoyeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+  }
+
   render() {
     return (
       <View>
@@ -16,4 +23,4 @@ class EmpoyeeList extends Component {
   }
 }
 
-export default EmpoyeeList;
+export default connect(null, { employeesFetch })(EmpoyeeList);
