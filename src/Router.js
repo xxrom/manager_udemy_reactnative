9b3b763d382f3516e95d8089bfc5afaceff4fa3a
Nikita Chernyshov <xpom55@mail.ru>
2017-10-24 20:07:09 +0300

    150, 151, ... 155 l/ employeeEdit employeeCreate create/update

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -24,6 +25,11 @@ const RouterComponent = () => {
           key="employeeCreate"
           component={EmployeeCreate}
           titel="Employee Create"
+        />
+        <Scene
+          key="employeeEdit"
+          component={EmployeeEdit}
+          titel="Employee Edit"
         />
       </Scene>
     </Router>
