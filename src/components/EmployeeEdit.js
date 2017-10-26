67aef774fb731497ff60c658848e1391a440e36b
Nikita Chernyshov <xpom55@mail.ru>
2017-10-26 19:45:35 +0300

    157 l/ add confirm component at common folder

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
@@ -23,13 +24,26 @@ class EmployeeEdit extends Component {
     });
   }
 
+  onTextPress() {
+    const { phone, shift } = this.props;
+
+    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)} >
             Save Changes
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            text Schedule
           </Button>
         </CardSection>
       </Card>
---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = () => {
+
+}
+
+export { Confirm };
