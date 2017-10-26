621d31b0f4060bc70fca88d7de16dc0a1dfb63b1
Nikita Chernyshov <xpom55@mail.ru>
2017-10-26 19:54:44 +0300

    158 l/ confirm filling all data

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() = {}}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
 
-}
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
 
 export { Confirm };
