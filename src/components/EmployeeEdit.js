680a4693183534e264b65f970f6a93b7d4075aac
Nikita Chernyshov <xpom55@mail.ru>
2017-10-26 20:36:46 +0300

    FINAL !!! 161 162 l/ working fire actions(delete)

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -33,7 +33,9 @@ class EmployeeEdit extends Component {
   }
 
   onAccept() {
+    const { uid } = this.props.employee;
 
+    this.props.employeeDelete({ uid });
   }
 
   onDecline() {
@@ -67,7 +69,6 @@ class EmployeeEdit extends Component {
           visible={this.state.showModal}
           onAccept={this.onAccept.bind(this)}
           onDecline={this.onDecline.bind(this)}
-
         >
           Are you sure you want to delete this?
         </Confirm>
@@ -82,4 +83,10 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps,
+  {
+    employeeUpdate,
+    employeeSave,
+    employeeDelete
+  }
+)(EmployeeEdit);
